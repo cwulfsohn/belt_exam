92006f9e98ad671c8b9ba98633e9d023c2dfcfcd
Curtis Wulfsohn <cwulfsohn26@gmail.com>
2017-02-23 17:53:03 -0600

    added logout

--- client/assets/controllers/homeController.js
+++ client/assets/controllers/homeController.js
@@ -1,4 +1,9 @@
-app.controller('homeController', function($scope, userFactory, $cookies){
+app.controller('homeController', function($scope, userFactory, $cookies, $location){
   $scope.username = $cookies.get('username')
   $scope.id = $cookies.get('id')
+  $scope.logout = function(){
+  $cookies.remove('username')
+  $cookies.remove('id')
+  $location.url('/')
+}
 })
